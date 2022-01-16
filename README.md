84b1f10a98cc254ab8ea4b08166ffc344f48b6dc
Jaswant Singh <jaswant_s@ch.iitr.ac.in>
2022-01-16 18:43:34 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# POKEMON Webapp
+
+## Overview
+
+Users will able to:
+- View the different pokemon cards and store there favorite pokemon cards at single place.
+
+
+## Built with
+
+- pokeapi
+- HTML
+- CSS properties
+- Javascript
+- React
+- Bootstrap
---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Header } from "./My-Components/Header";
+import { MainContent } from "./My-Components/MainContent";
+
+
+function App() {
+	return (
+		<>
+			<Header />
+			<MainContent />
+		</>
+	);
+}
+
+export default App;
